e6a7c8ed585b886b6a8c67a8545a91b5cd65ad8a
Tanush Sethi <tanushsethi55@gmail.com>
2022-09-14 17:25:19 +0530

    added fonts and data file

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,10 @@
 import React from "react";
+import dataArray from "../data";
 
 function Game() {
+
+    const [dataState, setDataState] = React.useState({});
+
     return (
         <div className="Game">
 
---
+++ src/data.js
@@ -0,0 +1,28 @@
+var data = [
+    {
+        id : 1,
+        number: 1
+    },
+    {
+        id: 2,
+        number: 1
+    },
+    {
+        id: 3,
+        number: 1
+    },
+    {
+        id: 4,
+        number: 1
+    },
+    {
+        id: 5,
+        number: 1
+    },
+    {
+        id: 6,
+        number: 1
+    },
+]
+
+export default data;
